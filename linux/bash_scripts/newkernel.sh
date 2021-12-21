460b274f54099d960a3e4f78f4c7f01e599aaebe
faradawn <73060648+faradawn@users.noreply.github.com>
2021-12-20 20:29:40 -0600

    Update newkernel.sh

--- linux/bash_scripts/newkernel.sh
+++ linux/bash_scripts/newkernel.sh
@@ -1,26 +1,22 @@
 mkdir -p mykernel
 cd mykernel
 sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
-wget https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-4.4.200.tar.gz
-tar xvf linux-4.4.200.tar.gz
+curl -O -k https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/linux-4.4.200.tar.gz
+tar xf linux-4.4.200.tar.gz
 cd linux-4.4.200
 cp -v /boot/config-$(uname -r) .config
 
-sleep 0.5
-echo "done downloading linux-4.4.200!"
-sleep 0.5
-echo "here are the later instructions after make menuconfig:"
-sleep 0.5
 
+echo "done!"
+sleep 0.5
+echo "next steps: make menuconfig"
+sleep 0.5
 echo "tmux new -s mysession"
-echo "make -j32"
+echo "sudo make -j32"
 echo "sudo make modules_install -j32"
 echo "sudo make install"
 echo "control b + d"
 echo "tmux a -t mysession"
 
 sleep 0.5
-echo -e "now ready to execute: make menucondig"
-sleep 0.5
-
-make menuconfig
+echo "happy hacking!"
