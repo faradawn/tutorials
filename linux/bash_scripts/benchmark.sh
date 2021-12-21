c3b22fa1175d7aa094828225291b27d147cca31b
faradawn <73060648+faradawn@users.noreply.github.com>
2021-12-20 21:42:52 -0600

    Update benchmark.sh

--- linux/bash_scripts/benchmark.sh
+++ linux/bash_scripts/benchmark.sh
@@ -10,8 +10,17 @@ gcc -o main main.c
 gcc -o create_file create_file.c
 chmod 700 counter.sh
 
+git clone https://github.com/hoytech/vmtouch.git
+cd vmtouch
+make
+sudo make install
+
+cd ..
+echo "ready to bench mark?"
+read var
+
 echo "./counter.sh 50 -i"
 ./counter.sh 50 -i
 
 
-# https://raw.githubusercontent.com/faradawn/tutorials/main/linux/bash_scripts/make.sh
+# source <(curl -s https://raw.githubusercontent.com/faradawn/tutorials/main/linux/bash_scripts/benchmark.sh)
