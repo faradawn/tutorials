3ee580c5f3066cfcbba8309d5c5bb4970082cc22
faradawn <faradawny@gmail,com>
2021-12-26 14:24:24 -0600

    working union check

---
+++ 05_Leetcode/graph/union_check_cycle.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <numeric>
+#include <unordered_map>
+
+
+using namespace std;
+
+typedef vector<int> Arr;
+int numV = 6;
+Arr *adj=new Arr[numV];
+
+typedef struct EdgeGraph{
+    int left;
+    int right;
+    EdgeGraph(int x, int y){left=x; right=y;}
+}EdgeGraph;
+
+void create_list(int s, vector<int> &arr){
+    for(auto &i : arr){
+        adj[s].push_back(i);
+    }
+}
+
+vector<EdgeGraph> create_edge_graph(vector<int> *adjList){
+    vector<EdgeGraph> edgeList;
+    unordered_map<int, int> myHash;
+    for(int i=0; i<numV; i++){
+        for(auto &j : adjList[i]){
+            if(myHash[j] != i){
+                edgeList.push_back(EdgeGraph(i, j));
+                myHash[i]=j;
+                cout<<"["<<i<<" - "<<j<<"] ";
+            }
+        }
+    }
+    return edgeList;
+}
+
+int findParent(int x, vector<int> &parents){
+    if(x == parents[x]){
+        return x;
+    }else{
+        return findParent(parents[x], parents);
+    }
+}
+
+void joinTwo(int x, int y, vector<int> &parents){
+    parents[findParent(x, parents)]=findParent(y, parents);
+}
+
+Arr createUnion(Arr *adjList){
+    unordered_map<int, int> visited;
+    Arr parents(numV);
+    iota(begin(parents), end(parents), 0);
+    for(int i=0; i<numV; i++){
+        for(auto &j : adjList[i]){
+            if(!visited[j]){
+                joinTwo(j, i, parents);
+                visited[j]=1;
+            }
+        }
+    }
+
+    return parents;
+}
+
+int main(){
+    Arr arr0={3,1};
+    Arr arr1={0};
+    Arr arr3={0,4};
+    Arr arr4={3};
+    Arr arr5={2};
+    Arr arr2={5};
+    create_list(0, arr0);
+    create_list(1, arr1);
+    create_list(2, arr2);
+    create_list(3, arr3);
+    create_list(4, arr4);
+    create_list(5, arr5);
+    
+    vector<EdgeGraph> edgeList = create_edge_graph(adj);
+}
