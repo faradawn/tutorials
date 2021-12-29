1b15c00bb2b87633323648e15e18c64811dd0cae
faradawn <faradawny@gmail,com>
2021-12-28 20:29:48 -0600

    done max heap and sort

---
+++ 05_Leetcode/tree/max_heap.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printArr(vector<int> arr){
+    for(int &i : arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+void swap(int *a, int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void heapify(vector<int> &arr, int i, int len){
+    int l=2*i+1;
+    int r=2*i+2;
+    int largest=i;
+    if(l<len && arr[l]>arr[largest]){
+        largest=l;
+    }
+    if(r<len && arr[r]>arr[largest]){
+        largest=r;
+    }
+    if(largest!=i){
+        swap(&arr[largest], &arr[i]);
+        heapify(arr, largest, len);
+    }
+}
+
+void heapSort(vector<int> &arr){
+    for(int n=arr.size()-1; n>0; n--){
+        swap(&arr[0], &arr[n]);
+        heapify(arr, 0, n);
+    }
+
+}
+
+int main(){
+    vector<int>arr={1, 12, 9, 5, 6, 10};
+    // build heap
+    int p=arr.size()/2-1;
+    for(int i=p; i>=0; i--){
+        heapify(arr, i, arr.size());
+    }
+    // sort heap
+    heapSort(arr);
+    printArr(arr);
+
+    
+
+}
