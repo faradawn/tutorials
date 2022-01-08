965625c2c4219432d3c117827672fb492293d242
faradawn <faradawny@gmail,com>
2022-01-08 16:47:29 -0600

    finished red black debug version

--- 05_Leetcode/tree/red_black_tree.cpp
+++ 05_Leetcode/tree/red_black_tree.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-enum Color{BLACK, RED};
+enum Color{RED, BLACK};
 
 typedef struct Node{
     int val;
@@ -50,7 +50,7 @@ void rotateLeft(Node *&root, Node *&pt){ // check pointer to reference
     Node* temp=y->left;
     pt->right=y->left; // cut y's right
     if(y){
-        temp->p=pt;
+        temp->p=pt; // jump
     }
 
     y->p=pt->p; // link y's parent
@@ -67,8 +67,7 @@ void rotateLeft(Node *&root, Node *&pt){ // check pointer to reference
     pt->p=y;
 }
 
-void rotateRight(Node *&root, Node *&pt)
-{
+void rotateRight(Node *&root, Node *&pt){
     Node *pt_left = pt->left;
  
     pt->left = pt_left->right;
@@ -95,24 +94,25 @@ void fixViolation(Node *&root, Node *&pt)
 {
     Node *parent_pt = NULL;
     Node *grand_parent_pt = NULL;
+    
+    cout<<"start: pt " <<pt->val <<"\n";
  
     while((pt != root) && (pt->color != BLACK)&&(pt->p->color == RED)){
+        cout<<"  iter: pt "<< pt->val << endl;
         parent_pt = pt->p;
         grand_parent_pt = pt->p->p;
  
-        /*  Case : A
-            Parent of pt is left child
-            of Grand-parent of pt */
         if (parent_pt == grand_parent_pt->left)
         {
- 
+
             Node *uncle = grand_parent_pt->right;
  
             /* Case : 1
                The uncle of pt is also red
                Only Recoloring required */
-            if (uncle != NULL && uncle->color==RED)
+            if (uncle && uncle->color==RED)
             {
+                cout<<"    L recolor: pt "<< pt->val<<"\n";
                 grand_parent_pt->color = RED;
                 parent_pt->color = BLACK;
                 uncle->color = BLACK;
@@ -126,6 +126,7 @@ void fixViolation(Node *&root, Node *&pt)
                    Left-rotation required */
                 if (pt == parent_pt->right)
                 {
+                    cout<<"    L rotated left: pt "<< pt->val<<"\n";
                     rotateLeft(root, parent_pt);
                     pt = parent_pt;
                     parent_pt = pt->p;
@@ -134,6 +135,7 @@ void fixViolation(Node *&root, Node *&pt)
                 /* Case : 3
                    pt is left child of its parent
                    Right-rotation required */
+                cout<<"    L rotated right: pt "<< pt->val<<"\n";
                 rotateRight(root, grand_parent_pt);
                 int temp=parent_pt->color;
                 parent_pt->color=grand_parent_pt->color;
@@ -152,9 +154,9 @@ void fixViolation(Node *&root, Node *&pt)
             /*  Case : 1
                 The uncle of pt is also red
                 Only Recoloring required */
-            if ((uncle != NULL) && (uncle->color ==
-                                                    RED))
+            if ((uncle != NULL) && (uncle->color ==RED))
             {
+                cout<<"    R recolor: pt "<< pt->val<<"\n";
                 grand_parent_pt->color = RED;
                 parent_pt->color = BLACK;
                 uncle->color = BLACK;
@@ -167,6 +169,7 @@ void fixViolation(Node *&root, Node *&pt)
                    Right-rotation required */
                 if (pt == parent_pt->left)
                 {
+                    cout<<"    R rotated right: pt "<< pt->val<<"\n";
                     rotateRight(root, parent_pt);
                     pt = parent_pt;
                     parent_pt = pt->p;
@@ -175,6 +178,7 @@ void fixViolation(Node *&root, Node *&pt)
                 /* Case : 3
                    pt is right child of its parent
                    Left-rotation required */
+                cout<<"    R rotated left: pt "<< pt->val<<"\n";
                 rotateLeft(root, grand_parent_pt);
                 int temp=parent_pt->color;
                 parent_pt->color=grand_parent_pt->color;
@@ -183,25 +187,27 @@ void fixViolation(Node *&root, Node *&pt)
             }
         }
     }
- 
-    root->color = BLACK;
+
+    root->color=BLACK;
+    cout<<"end: root " << root->val << (root->color ? " Black" : " Red") << "; pt " << pt->val << (pt->color ? " Black" : " Red") << "\n\n";
 }
 
 Node* insert(vector<int> &vec){
     Node* root=new Node(vec[0]);
+    root->color=BLACK;
+    Node* newNode;
     for(int i=1; i<vec.size(); i++){
-        Node* newNode=NULL;
         bstInsert(root, vec[i], newNode);
         fixViolation(root, newNode);
     }
-    
-    
 
     return root;
 }
 
 int main(){
-    vector<int> vec={11,2,14,1};
+    cout<<"Black is "<< BLACK <<endl;
+    // vector<int> vec={11,2,1,7,5,4,8,14,15};
+    vector<int> vec={26,18,7,10,3,22,8,11};
     Node* root=insert(vec);
     printBT("",root,0);
 
