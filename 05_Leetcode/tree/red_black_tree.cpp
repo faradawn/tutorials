68e11ed51a431e77a41a5efaa6a90a24eca2f0f8
faradawn <faradawny@gmail,com>
2022-01-07 21:57:39 -0600

    working rotateLeft

--- 05_Leetcode/tree/red_black_tree.cpp
+++ 05_Leetcode/tree/red_black_tree.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -18,30 +19,62 @@ void printBT(const string& prefix, const Node* node, int isLeft)
 {
     if(!node){return;}
     cout << prefix;
-    cout << (isLeft ? "├──" : "└──" );
-    cout << node->val << endl;
+    if(node->color==BLACK){
+        cout << (isLeft ? "├──[" : "└──[" ) << node->val <<"]\n";
+    }else{
+        cout << (isLeft ? "├── " : "└── " ) << node->val <<" \n";
+    }
     printBT( prefix + (isLeft ? "│   " : "    "), node->left, 1);
     printBT( prefix + (isLeft ? "│   " : "    "), node->right, 0);
 }
 
-Node* bstInsert(Node* node, int x){
+Node* bstInsert(Node* node, int x){ // only inserts not duplicate elements
     if(!node){
         return new Node(x);
     }
-    if(x<=node->val){
+    if(x<node->val){
         node->left=bstInsert(node->left, x);
-    }else{
+        node->left->p=node;
+        
+    }else if(x>node->val){
         node->right=bstInsert(node->right, x);
+        node->right->p=node;
     }
     return node;
 }
 
-int main(){
-    int arr[]={4,1,5,6,2,3,7};
-    Node* node=new Node(arr[0]);
-    for(int i=1; i<sizeof(arr)/sizeof(arr[0]); i++){
-        bstInsert(node, arr[i]);
+void rotateLeft(Node*& root, Node*& pt){
+    Node* y=pt->right;
+    Node* temp=y->left;
+    pt->right=temp; // cut y's right
+    if(temp){
+        temp->p=pt;
     }
-    printBT("",node,0);
+    y->p=pt->p; // link y's parent
+    if(!pt->p){ // update root
+        root=y;
+    }else if(pt==pt->p->left){
+        pt->p->left=y;
+    }else{
+        pt->p->right=y;
+    }
+    y->left=pt; // link y's left
+    pt->p=y;
+
+}
+
+Node* insert(vector<int> &vec){
+    Node* root=new Node(vec[0]);
+    for(int &i : vec){
+        bstInsert(root, i);
+    }
+    // rotateLeft(root, root->left->right);
+    return root;
+}
+
+int main(){
+    vector<int> vec={11,2,14,1,7,15,5,8,4};
+    Node* root=insert(vec);
+    printBT("",root,0);
 
 }
