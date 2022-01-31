c5a41c74b2d7cdda038e2341734d148fa08a3723
faradawn <73060648+faradawn@users.noreply.github.com>
2022-01-30 21:58:51 -0600

    Add files via upload

---
+++ linux/cortx/kube.sh
@@ -0,0 +1,19 @@
+# install 
+
+sudo su 
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum check-update
+y | yum install -y yum-utils device-mapper-persistent-data lvm2 firewalld docker kubelet kubeadm kubectl
+
+systemctl enable docker && systemctl start docker
+systemctl enable kubelet && systemctl start kubelet
