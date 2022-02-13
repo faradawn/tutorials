e5144e2195e1b70805cdeccb9d544bc262df2932
faradawn <73060648+faradawn@users.noreply.github.com>
2022-02-12 22:52:10 -0600

    Update kube.sh

--- linux/cortx/kube.sh
+++ linux/cortx/kube.sh
@@ -110,14 +110,15 @@ sleep 3
 
 if [[ $ME -eq "master" ]]
 then
-echo "kubeadm init"
-kubeadm init
-sleep 1
-mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config
-export kubever=$(kubectl version | base64 | tr -d '\n')
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
-sleep 3
-kubectl get nodes
+  echo "kubeadm init"
+  kubeadm init
+  sleep 1
+  mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config
+  export kubever=$(kubectl version | base64 | tr -d '\n')
+  kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+  sleep 3
+  kubectl get nodes
+fi
 
 sleep 3
 echo -e '\n === done, congrats! === \n'
