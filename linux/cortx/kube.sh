428dcfdddbabf2e6a6785f976f57044131d6dd1d
faradawn <73060648+faradawn@users.noreply.github.com>
2022-05-03 21:10:29 -0500

    Update kube.sh

--- linux/cortx/kube.sh
+++ linux/cortx/kube.sh
@@ -8,16 +8,22 @@ options=("master" "other" "quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "master")
-            echo "set-hostname cortx-storage-1"
-            hostnamectl set-hostname cortx-storage-1
-            ME="master"
+        "node-1")
+            echo "set-hostname node-1"
+            hostnamectl set-hostname node-1
             sleep 1
             break
             ;;
-        "other")
-            echo "hostnamectl set-hostname cortx-storage-n"
-            ME="other"
+        "node-2")
+            echo "hostnamectl set-hostname node-2"
+            hostnamectl set-hostname node-2
+            sleep 1
+            break
+            ;;
+        "master")
+            echo "set-hostname master"
+            hostnamectl set-hostname master
+            ME="master"
             sleep 1
             break
             ;;
@@ -47,33 +53,17 @@ sudo yum -y update && sudo yum -y install kubelet kubeadm kubectl
 
 # install docker
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce
 
-sudo mkdir /etc/docker && sudo mkdir -p /etc/systemd/system/docker.service.d
-
-sudo tee /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF
-
-sudo systemctl daemon-reload && sudo systemctl restart docker && sudo systemctl enable docker
+sudo systemctl start docker && sudo systemctl enable docker
 sudo systemctl enable kubelet
 
 
 
 echo -e '\n === Part2: configure DNS and disable SElinux === \n'
-cat <<EOF>> /etc/hosts
-10.52.0.193 cortx-storage-1
-10.52.1.106 cortx-storage-4
+cat <<EOF> /etc/hosts
+10.52.0.193 node-1
+10.52.1.106 master
 EOF
 
 # disable SElinx
@@ -89,7 +79,6 @@ sudo modprobe br_netfilter
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
 EOF
 
 sudo sysctl --system
@@ -116,4 +105,4 @@ then
 fi
 
 echo -e '\n === done! git clone cortx now === \n'
-git clone https://github.com/Seagate/cortx-k8s; cd cortx-k8s/k8_cortx_cloud; vi solution.yaml
+git clone https://github.com/Seagate/cortx-k8s; cd cortx-k8s/k8_cortx_cloud
