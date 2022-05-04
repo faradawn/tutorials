f5f69b78be8a3702f5ec64a1779632b1e53b7f41
faradawn <73060648+faradawn@users.noreply.github.com>
2022-05-03 20:39:38 -0500

    Update kube.sh

--- linux/cortx/kube.sh
+++ linux/cortx/kube.sh
@@ -1,40 +1,23 @@
 # source <(curl -s https://raw.githubusercontent.com/faradawn/tutorials/main/linux/cortx/kube.sh)
-# private ip: ifconfig -a, public ip: curl ifconfig.me
-# change line 85 and 119
 
-echo 'Welcome V4! Make sure you are root!'
+echo 'Welcome! Make sure you are root!'
 
 ME="NULL"
 PS3='Please enter your choice: '
-options=("node-1" "node-2" "node-3" "master" "quit")
+options=("master" "other" "quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "master")
-            echo "set-hostname master"
-            hostnamectl set-hostname master
+            echo "set-hostname cortx-storage-1"
+            hostnamectl set-hostname cortx-storage-1
             ME="master"
             sleep 1
             break
             ;;
-        "node-1")
-            echo "set-hostname node-1"
-            hostnamectl set-hostname node-1
-            ME="worker-1"
-            sleep 1
-            break
-            ;;
-        "node-2")
-            echo "set-hostname node-2"
-            hostnamectl set-hostname node-2
-            ME="worker-2"
-            sleep 1
-            break
-            ;;
-        "node-3")
-            echo "set-hostname node-3"
-            hostnamectl set-hostname node-3
-            ME="worker-3"
+        "other")
+            echo "hostnamectl set-hostname cortx-storage-n"
+            ME="other"
             sleep 1
             break
             ;;
@@ -46,7 +29,6 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-
 
 
 echo -e '\n === Part1: install kubernetes and docker === \n'
@@ -90,12 +72,8 @@ sudo systemctl enable kubelet
 
 echo -e '\n === Part2: configure DNS and disable SElinux === \n'
 cat <<EOF>> /etc/hosts
-10.52.2.235 master
-10.52.3.92 node-1
-10.52.2.250 node-2
-10.52.3.162 node-3
-
-
+10.52.0.193 cortx-storage-1
+10.52.1.106 cortx-storage-4
 EOF
 
 # disable SElinx
@@ -127,7 +105,6 @@ then
     sudo kubeadm init \
       --pod-network-cidr=192.168.0.0/16 \
       --upload-certs \
-      --control-plane-endpoint=10.52.2.235
 
     mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
