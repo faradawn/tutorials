2d67c220341fefe7e20759e308ae2f1294d9f5a9
faradawn <73060648+faradawn@users.noreply.github.com>
2022-07-23 15:32:50 -0500

    new merge sort with one arr

--- leetcode/sort/merge_sort.cpp
+++ leetcode/sort/merge_sort.cpp
@@ -1,3 +1,40 @@
+// New merge sort with arr
+public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        int *res = new int[r-l+1];
+        int i = l, j = m+1, k = 0;
+        while(i <= m && j <= r){
+            if(arr[i] < arr[j]){
+                res[k++] = arr[i++];
+            }else{
+                res[k++] = arr[j++];
+            }
+        }
+        while(i <= m)
+            res[k++] = arr[i++];
+        while(j <= r){
+            res[k++] = arr[j++];
+        }
+        
+         for(int a = 0; a < k; a++){
+             arr[l+a] = res[a];
+         }    
+    }
+
+    void mergeSort(int arr[], int l, int r)
+    {
+        if(r <= l)
+            return;
+            
+        int m = l + (r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+
+
+// Old merge sort
 #include <iostream>
 #include <vector>
 #include "helper.h"
